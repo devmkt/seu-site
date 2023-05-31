294504c59cbe18596ebe8b098f9131f9bd3ba53a
Lereno <lereno.soares@gmail.com>
2023-05-30 23:10:03 -0300

    v1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,37 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svg}'],
+	safelist:[{
+		pattern: /w-([1-9]|10|11|12$)\/12/
+	}],
 	theme: {
-		extend: {},
+		screens: {
+			'xs': '376px',
+			'sm': '480px',
+			'md': '768px',
+			'lg': '976px',
+			'xl': '1440px',
+		},
+		extend: {
+			keyframes: {
+				'in-by-left': {
+				  '0%': { transform: 'translateX(-100%)' },
+				  '100%': { transform: 'translateX(0)' },
+				},
+				'fade-in': {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
+			},
+			animation: {
+				'in-by-left': 'in-by-left 3s ease-out',
+				'fade-in': 'fade-in 2s ease-out'
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
-		require("daisyui")
+		require("daisyui"),
+		require('tailwindcss-patterns')
 	],
 	darkMode: ['class', '[data-theme="dark"]'],
 	daisyui: {
